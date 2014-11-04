e90e405003c4ef8edb629c59d7684299cd850677
Brett Lawson <brett19@gmail.com>
2014-11-04 16:57:39 -0400

    Release 2.0.1.

--- php_couchbase.h
+++ php_couchbase.h
@@ -1,7 +1,7 @@
 #ifndef PHP_COUCHBASE_H_
 #define PHP_COUCHBASE_H_
 
-#define PHP_COUCHBASE_VERSION "2.0.0"
+#define PHP_COUCHBASE_VERSION "2.0.1"
 #define PHP_COUCHBASE_EXTNAME "couchbase"
 
 extern zend_module_entry couchbase_module_entry;
