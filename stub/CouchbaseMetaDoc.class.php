28a85f9b2734f9038e79430855b21c45a9a52d2c
Elias Van Ootegem <elias.vanootegem@wimionline.be>
2015-02-17 14:30:57 +0100

    Add stubs for CouchbaseMetaDoc & CouchbaseException classes, too

---
+++ stub/CouchbaseMetaDoc.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class CouchbaseMetaDoc
+ * This class has no methods, but is used by CouchbaseBucket
+ * Represents a couchbase document & meta
+ *
+ * @package Couchbase
+ */
+class CouchbaseMetaDoc
+{
+    public $error = null;
+    public $value = null;
+    public $flags = null;
+    public $case = null;
+}
